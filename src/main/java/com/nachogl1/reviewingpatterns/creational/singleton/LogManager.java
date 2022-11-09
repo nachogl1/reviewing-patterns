1d4c6a6835ee33aa66a6a7f22841e78426ba8ce9
Ignacio Gonzalez <ignacio.gonzalez@codurance.com>
2022-11-09 12:25:55 +0000

    Updating Singleton Pattern

---
+++ src/main/java/com/nachogl1/reviewingpatterns/creational/singleton/LogManager.java
@@ -0,0 +1,32 @@
+package com.nachogl1.reviewingpatterns.creational.singleton;
+
+public class LogManager {
+
+    private static LogManager instance;
+
+    private LogManager() {
+    }
+
+    public static LogManager getInstance() {
+        if (instance == null) instance = new LogManager();
+
+        return instance;
+    }
+
+    public void debug(String message) {
+        System.out.println("DEBUG: " + message);
+    }
+
+    public void info(String message) {
+        System.out.println("INFO: " + message);
+    }
+
+    public void warn(String message) {
+        System.out.println("WARN: " + message);
+    }
+
+    public void error(String message) {
+        System.out.println("ERROR: " + message);
+    }
+
+}
